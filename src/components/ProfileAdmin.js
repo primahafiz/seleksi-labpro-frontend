250d2bb33e385248ba3034d1550755cd265dfeeb
primahafiz <primahafiz@gmail.com>
2022-07-29 22:18:17 +0700

    frontend backend all

--- src/components/ProfileAdmin.js
+++ src/components/ProfileAdmin.js
@@ -62,7 +62,7 @@ function ProfileAdmin() {
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link text-white font-weight-bold" href="/admin">Home <span className="sr-only">(current)</span></a>
+                  <a className="nav-link text-white" href="/admin">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link text-white" href="/admin/verify-registration">Regis Data</a>
