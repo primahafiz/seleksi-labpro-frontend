05c47e20bf22df45cf0bbdebd6a3a7e47075376b
primahafiz <primahafiz@gmail.com>
2022-07-31 22:45:31 +0700

    debug

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use(
-    '/',
+    '/api',
     createProxyMiddleware({
       target: 'https://api-bnmo.herokuapp.com',
       changeOrigin: true,
